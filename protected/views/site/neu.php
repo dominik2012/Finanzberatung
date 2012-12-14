99f0ae417af6df316b9a1951a966fd12a6685583
dominik2012 <dominik.hopf@stud.uni-regensburg.de>
2012-12-14 12:45:20 +0100

    Filterübersichtsanzeige

--- protected/views/site/neu.php
+++ protected/views/site/neu.php
@@ -414,16 +414,118 @@
                 <tr>
                     <td><!--SQL: <?php if(isset($fil)){echo $fil;}?></td>-->
                     
-                    <td>GP: <?php if(isset($fil)){echo $model4;}?></td>
-                    <td>UP: <?php if(isset($fil)){echo $unterphase;}?></td>
-                    <td>Fkt: <?php if(isset($fil)){echo $name;}?></td>
-                    <td>PmB: <?php if(isset($fil)){echo $privmb;}?></td>
-                    <td>PoB: <?php if(isset($fil)){echo $privob;}?></td>
-                    <td>PromB: <?php if(isset($fil)){echo $profmb;}?></td>
-                    <td>PromB: <?php if(isset($fil)){echo $profob;}?></td>
-                    <td>RAG: <?php if(isset($fil)){echo $rausfg;}?></td>
-                    <td>HSR(z): <?php if(isset($fil)){echo $hsrz;}?></td>
-                    <td>HSR(g): <?php if(isset($fil)){echo $hsra;}?></td>
+                    <td>GP: <?php 
+                                if(isset($fil)){
+                                    if($model4 == "0,1,2,3,4,5,6,7,8,9,10"){
+                                        echo 'all';
+                                    }else{
+                                        echo $model4;
+                                    }
+                                   
+                                }
+                            ?>
+                    </td>
+                    
+                    <td>UP: <?php 
+                                if(isset($fil)){
+                                    if($unterphase == "0,1,2,3,4,5,6,7,8,9,10,11"){
+                                        echo 'all';
+                                    }else{
+                                        echo $unterphase;
+                                    }
+                                   
+                                }
+                            ?>
+                    </td>
+                    
+                    <td>Fkt: <?php 
+                                if(isset($fil)){
+                                    if($name == "0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107"){
+                                        echo 'all';
+                                    }else{
+                                        echo $name;
+                                    }
+                                   
+                                }
+                            ?>
+                    </td>
+                    <td>PmB: <?php 
+                                if(isset($fil)){
+                                    if($privmb == "'gesetzFunktion','keine Abhängig','funktionGesetz'"){
+                                        echo 'all';
+                                    }else{
+                                        echo $privmb;
+                                    }
+                                   
+                                }
+                            ?>
+                    </td>
+                    <td>PoB: <?php 
+                                if(isset($fil)){
+                                    if($privob == "'gesetzFunktion','keine Abhängig','funktionGesetz'"){
+                                        echo 'all';
+                                    }else{
+                                        echo $privob;
+                                    }
+                                   
+                                }
+                            ?>
+                    </td>
+                    <td>PromB: <?php 
+                                if(isset($fil)){
+                                    if($profmb == "'gesetzFunktion','keine Abhängig','funktionGesetz'"){
+                                        echo 'all';
+                                    }else{
+                                        echo $profmb;
+                                    }
+                                   
+                                }
+                            ?>
+                    </td>
+                    <td>PromB: <?php 
+                                if(isset($fil)){
+                                    if($profob == "'gesetzFunktion','keine Abhängig','funktionGesetz'"){
+                                        echo 'all';
+                                    }else{
+                                        echo $profob;
+                                    }
+                                   
+                                }
+                            ?>
+                    </td>
+                    <td>RAG: <?php 
+                                if(isset($fil)){
+                                    if($rausfg == "'gesetzFunktion','keine Abhängig','funktionGesetz'"){
+                                        echo 'all';
+                                    }else{
+                                        echo $rausfg;
+                                    }
+                                   
+                                }
+                            ?>
+                    </td>
+                    <td>HSR(z): <?php 
+                                if(isset($fil)){
+                                    if($hsrz == "'gelb','gruen','gelbHoch'"){
+                                        echo 'all';
+                                    }else{
+                                        echo $hsrz;
+                                    }
+                                   
+                                }
+                            ?>
+                    </td>
+                    <td>HSR(g): <?php 
+                                if(isset($fil)){
+                                    if($hsra == "'gruen','gelb'"){
+                                        echo 'all';
+                                    }else{
+                                        echo $hsra;
+                                    }
+                                   
+                                }
+                            ?>
+                    </td>
                     
                 </tr>
             </tbody>
