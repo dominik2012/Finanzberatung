419f8af778507fdc6de0bc49e110baefc60e7de1
dominik2012 <dominik.hopf@stud.uni-regensburg.de>
2012-12-14 12:58:21 +0100

    Filterübersichtsanzeige: Update 1

--- protected/views/site/neu.php
+++ protected/views/site/neu.php
@@ -414,113 +414,103 @@
                 <tr>
                     <td><!--SQL: <?php if(isset($fil)){echo $fil;}?></td>-->
                     
-                    <td>GP: <?php 
+                    <td><?php 
                                 if(isset($fil)){
-                                    if($model4 == "0,1,2,3,4,5,6,7,8,9,10"){
-                                        echo 'all';
+                                    if($model4 == "0,1,2,3,4,5,6,7,8,9,10"){ 
                                     }else{
-                                        echo $model4;
+                                        echo '<p style="color: #0075B8;">GROPHASE: '.$model4.'</p>';
                                     }
                                    
                                 }
                             ?>
                     </td>
                     
-                    <td>UP: <?php 
+                    <td><?php 
                                 if(isset($fil)){
                                     if($unterphase == "0,1,2,3,4,5,6,7,8,9,10,11"){
-                                        echo 'all';
                                     }else{
-                                        echo $unterphase;
+                                        echo '<p style="color: #0075B8;">UNTERPHASE: '.$unterphase.'</p>';
                                     }
                                    
                                 }
                             ?>
                     </td>
                     
-                    <td>Fkt: <?php 
+                    <td><?php 
                                 if(isset($fil)){
                                     if($name == "0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,101,102,103,104,105,106,107"){
-                                        echo 'all';
                                     }else{
-                                        echo $name;
+                                        echo '<p style="color: #0075B8;">FUNKTION: '.$name.'</p>';
                                     }
                                    
                                 }
                             ?>
                     </td>
-                    <td>PmB: <?php 
+                    <td><?php 
                                 if(isset($fil)){
                                     if($privmb == "'gesetzFunktion','keine Abhängig','funktionGesetz'"){
-                                        echo 'all';
                                     }else{
-                                        echo $privmb;
+                                        echo '<p style="color: #0075B8;">PRIVAT MIT BERATUNG: '.$privmb.'</p>';
                                     }
                                    
                                 }
                             ?>
                     </td>
-                    <td>PoB: <?php 
+                    <td><?php 
                                 if(isset($fil)){
                                     if($privob == "'gesetzFunktion','keine Abhängig','funktionGesetz'"){
-                                        echo 'all';
                                     }else{
-                                        echo $privob;
+                                        echo '<p style="color: #0075B8;">PRIVAT OHNE BERATUNG: '.$privob.'</p>';
                                     }
                                    
                                 }
                             ?>
                     </td>
-                    <td>PromB: <?php 
+                    <td><?php 
                                 if(isset($fil)){
                                     if($profmb == "'gesetzFunktion','keine Abhängig','funktionGesetz'"){
-                                        echo 'all';
                                     }else{
-                                        echo $profmb;
+                                        echo '<p style="color: #0075B8;">PROFESSIONELL OHNE BERATUNG: '.$profmb.'</p>';
                                     }
                                    
                                 }
                             ?>
                     </td>
-                    <td>PromB: <?php 
+                    <td><?php 
                                 if(isset($fil)){
                                     if($profob == "'gesetzFunktion','keine Abhängig','funktionGesetz'"){
-                                        echo 'all';
                                     }else{
-                                        echo $profob;
+                                        echo '<p style="color: #0075B8;">PROFESSIONELL MIT BERATUNG: '.$profob.'</p>';
                                     }
                                    
                                 }
                             ?>
                     </td>
-                    <td>RAG: <?php 
+                    <td><?php 
                                 if(isset($fil)){
                                     if($rausfg == "'gesetzFunktion','keine Abhängig','funktionGesetz'"){
-                                        echo 'all';
                                     }else{
-                                        echo $rausfg;
+                                        echo '<p style="color: #0075B8;">REINES AUSFÜHRUNGSGESCHÄFT: '.$rausfg.'</p>';
                                     }
                                    
                                 }
                             ?>
                     </td>
-                    <td>HSR(z): <?php 
+                    <td><?php 
                                 if(isset($fil)){
                                     if($hsrz == "'gelb','gruen','gelbHoch'"){
-                                        echo 'all';
                                     }else{
-                                        echo $hsrz;
+                                        echo '<p style="color: #0075B8;">HANDLUNGSSPIELRAUM ZUKÜNFTIG: '.$hsrz.'</p>';
                                     }
                                    
                                 }
                             ?>
                     </td>
-                    <td>HSR(g): <?php 
+                    <td> <?php 
                                 if(isset($fil)){
                                     if($hsra == "'gruen','gelb'"){
-                                        echo 'all';
                                     }else{
-                                        echo $hsra;
+                                        echo '<p style="color: #0075B8;">HANDLUNGSSPIELRAUM GEGENWART: '.$hsra.'</p>';
                                     }
                                    
                                 }
